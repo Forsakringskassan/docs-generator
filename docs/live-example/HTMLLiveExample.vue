2d1d072789fa2b20dbe69ca8dcfb3549b48e7c56
David Sveningsson <ext@sidvind.com>
2024-04-13 20:06:08 +0200

    docs: include docs-live-example docs in this site

---
+++ docs/live-example/HTMLLiveExample.vue
@@ -0,0 +1,30 @@
+<template>
+    <live-example language="html" :template>
+        <label class="docs-block" for="html-name"> Name </label>
+        <select id="html-name" v-model="name" class="docs-block" name="html-name">
+            <option value="World">World</option>
+            <option value="Kalle Anka">Kalle Anka</option>
+            <option value="Fred Flintstone">Fred Flintstone</option>
+        </select>
+    </live-example>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { LiveExample } from "@forsakringskassan/docs-live-example";
+
+export default defineComponent({
+    name: "HTMLLiveExample",
+    components: { LiveExample },
+    data() {
+        return {
+            name: "World",
+        };
+    },
+    computed: {
+        template(): string {
+            return /* HTML */ ` <div>Hello ${this.name}!</div> `;
+        },
+    },
+});
+</script>
