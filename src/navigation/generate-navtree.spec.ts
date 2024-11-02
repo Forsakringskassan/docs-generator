aec08fa318eb1fd828849fcb2648e7b68c3b841d
David Sveningsson <ext@sidvind.com>
2024-11-02 17:49:18 +0100

    fix: only match filenames when matching source files

--- src/navigation/generate-navtree.spec.ts
+++ src/navigation/generate-navtree.spec.ts
@@ -428,7 +428,10 @@ async function getDocsFromGlob(
     pattern: string,
     basePath: string,
 ): Promise<Document[]> {
-    const filenames = await glob(pattern, { ignore: "./docs/node_modules/**" });
+    const filenames = await glob(pattern, {
+        ignore: "./docs/node_modules/**",
+        nodir: true,
+    });
     const parsed = filenames.map((it) => frontMatterFileReader(it, basePath));
     return (await Promise.all(parsed)).flat();
 }
