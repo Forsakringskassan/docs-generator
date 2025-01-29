541c2c9086fc2305ae090477c7ca153e8937114a
David Sveningsson <ext@sidvind.com>
2025-01-29 23:13:02 +0100

    refactor: use a regex to silence security warning

--- src/render/markdown/utils/replace-at-link.ts
+++ src/render/markdown/utils/replace-at-link.ts
@@ -1,5 +1,5 @@
 export function replaceAtLink(text: string): string {
     return text.replace(/{@link [^}]+[$}]/g, (match) =>
-        match.replace("{", "&lcub;").replace("}", "&rcub;"),
+        match.replace(/^{/, "&lcub;").replace(/}$/, "&rcub;"),
     );
 }
