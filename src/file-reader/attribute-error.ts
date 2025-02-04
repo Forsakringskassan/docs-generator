7a95aee01e931ca56f6d8f2451a25de2bd5396e2
David Sveningsson <ext@sidvind.com>
2025-02-04 08:27:01 +0100

    feat: frontmatter attribute basic validation

---
+++ src/file-reader/attribute-error.ts
@@ -0,0 +1,83 @@
+import { type SourceLocation, codeFrameColumns } from "@babel/code-frame";
+import { closest, distance } from "fastest-levenshtein";
+import { documentAttributeKeys } from "../document";
+
+interface AttributeErrorOptions {
+    attribute: string;
+    filePath: string;
+    content: string;
+}
+
+const threshold = 3;
+
+/**
+ * Hack to guess the line in the frontmatter block, it makes horrible
+ * assumptions and will break down if anything than a simple flat structure is
+ * used with attribute validation but it works good enough for now.
+ *
+ * Nested keys could be fixed by searching recursive though.
+ *
+ * @internal
+ */
+export function findLocation(
+    content: string,
+    attr: string,
+): SourceLocation | null {
+    const match = content.match(/^---\n(.*?)^---/ms);
+    if (!match || match.index !== 0) {
+        return null;
+    }
+
+    /* @todo would be better to actually parse the yaml block to an ast and get the location from there */
+    const yaml = match[1];
+    const lines = yaml.split("\n");
+    const index = lines.findIndex((it) => it.startsWith(`${attr}:`)); // this makes horrible assumptions and will break
+    if (index < 0) {
+        return null;
+    }
+
+    const blockBegin = 2; // the first line of the frontmatter block is at line 2
+    return {
+        start: { line: index + blockBegin, column: 1 },
+        end: { line: index + blockBegin, column: 1 + attr.length },
+    };
+}
+
+/**
+ * @internal
+ */
+export class AttributeError extends Error {
+    private readonly attribute: string;
+    private readonly content: string;
+
+    public constructor(options: AttributeErrorOptions) {
+        const { attribute, filePath, content } = options;
+        super(`Unknown attribute "${attribute}" in "${filePath}"`);
+        this.name = "AttributeError";
+        this.attribute = attribute;
+        this.content = content;
+    }
+
+    public prettyError(): string {
+        const { attribute, content, message } = this;
+
+        const closestKey = closest(attribute, documentAttributeKeys);
+        const d = distance(attribute, closestKey);
+        const suggestion =
+            d <= threshold
+                ? `Did you mean to use "${closestKey}"?`
+                : `Unknown attribute "${attribute}"`;
+
+        const location = findLocation(content, attribute);
+        if (location) {
+            const codeframe = codeFrameColumns(content, location, {
+                message: suggestion,
+            });
+            return [message, "", codeframe].join("\n");
+        } else if (d <= threshold) {
+            return [message, "", suggestion].join("\n");
+        } else {
+            return message;
+        }
+    }
+}
