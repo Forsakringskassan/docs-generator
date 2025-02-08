0849fa049d6e80507308b0b5284244f7c67c32e4
David Sveningsson <ext@sidvind.com>
2025-02-08 13:35:42 +0100

    refactor: move document.ts to folder

---
+++ src/document/index.ts
@@ -0,0 +1,9 @@
+export {
+    type Component,
+    type Document,
+    type DocumentAttributes,
+    type DocumentBadge,
+    type FileInfo,
+    type NormalizedDocumentAttributes,
+    documentAttributeKeys,
+} from "./document";
