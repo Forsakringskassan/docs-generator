8304d16ab15f5e01c989b3d3e64e3e49ca1ee86a
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-03-11 13:52:36 +0100

    fix: properly pass assets from importmap to compiled examples

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
+export { type AssetInfo, type AssetImportmap } from "./asset-info";
 export { type CompileOptions } from "./compile-options";
 export { cssAssetProcessor, type CSSAsset } from "./css-asset-processor";
 export { jsAssetProcessor, type JSAsset } from "./js-asset-processor";
