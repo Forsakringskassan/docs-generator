e3046f96501d850472c69d7f2b0c5cd49cad4c59
Olof Fredriksson <oloflolof@gmail.com>
2025-03-26 16:40:45 +0100

    feat: using sass-module-importer to find sass files

--- src/sass/compile-sass-string.ts
+++ src/sass/compile-sass-string.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import { compileStringAsync, NodePackageImporter } from "sass";
-import { moduleImporter } from "./module-importer";
+import { moduleImporter } from "@forsakringskassan/sass-module-importer";
 
 /**
  * @public
@@ -11,12 +11,10 @@ import { moduleImporter } from "./module-importer";
 export async function compileSassString(
     dst: string,
     style: string,
-    options: { cwd?: string } = {},
 ): Promise<void> {
-    const { cwd = process.cwd() } = options;
     const result = await compileStringAsync(style, {
         style: "expanded",
-        importers: [new NodePackageImporter(), moduleImporter({ cwd })],
+        importers: [new NodePackageImporter(), moduleImporter],
     });
     await fs.mkdir(path.dirname(dst), { recursive: true });
     await fs.writeFile(dst, result.css, "utf-8");
