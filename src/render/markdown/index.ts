0fe71ac2a7337671bef6259b607c3a97ab331656
David Sveningsson <ext@sidvind.com>
2025-04-02 01:03:40 +0200

    fix: remove reset on anchor tags

--- src/render/markdown/index.ts
+++ src/render/markdown/index.ts
@@ -2,5 +2,6 @@ export { codePreview } from "./code-preview";
 export { headingLevel } from "./heading-level";
 export { imageResources } from "./image-resources";
 export { containerRenderer } from "./container-renderer";
+export { paragraph } from "./paragraph";
 export { table } from "./table";
 export { codeInline } from "./code-inline";
