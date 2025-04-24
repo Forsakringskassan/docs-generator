c7d41ff07c72aa9e0f1b86a460841fd7104806d0
David Sveningsson <ext@sidvind.com>
2025-04-24 23:22:27 +0200

    fix: fix list items being rendered with paragraph tag

--- src/render/markdown/paragraph.ts
+++ src/render/markdown/paragraph.ts
@@ -1,10 +1,18 @@
 import type MarkdownIt from "markdown-it";
 
 export function paragraph(): (md: MarkdownIt) => void {
+    const renderToken: MarkdownIt.Renderer.RenderRule = (...args) => {
+        const [tokens, idx, options, , self] = args;
+        return self.renderToken(tokens, idx, options);
+    };
+
     return function (md: MarkdownIt): void {
         /* eslint-disable camelcase -- property is defined in upstream library */
-        md.renderer.rules.paragraph_open = () => `<p class="docs-paragraph">`;
-        md.renderer.rules.paragraph_close = () => `</p>`;
+        const renderRule = md.renderer.rules.paragraph_open ?? renderToken;
+        md.renderer.rules.paragraph_open = (tokens, idx, index, env, self) => {
+            tokens[idx].attrJoin("class", "docs-paragraph");
+            return renderRule(tokens, idx, index, env, self);
+        };
         /* eslint-enable camelcase */
     };
 }
