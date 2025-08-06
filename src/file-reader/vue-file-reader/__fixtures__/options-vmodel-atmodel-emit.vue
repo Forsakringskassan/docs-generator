f39a83e6f8339da93041e47a5d1759e4227ae651
Wei Nicklas (66136294) <nicklas.wei@forsakringskassan.se>
2025-08-06 10:38:34 +0200

    feat: add separate v-model table (refs SFKUI-7107)

---
+++ src/file-reader/vue-file-reader/__fixtures__/options-vmodel-atmodel-emit.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        /**
+         * Lorem ipsum.
+         *
+         * @model
+         */
+        modelValue: {
+            type: String,
+            default: "",
+        },
+    },
+    emits: ["update:modelValue"],
+});
+</script>
