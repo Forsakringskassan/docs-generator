9b4889f86f8941a06162a9f277cddcf5bca100f2
David Sveningsson <ext@sidvind.com>
2025-12-12 09:13:57 +0100

    test: add test for `(typeof foo)[number]`

---
+++ src/file-reader/vue-file-reader/__fixtures__/options-props-enum.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { type PropType, defineComponent } from "vue";
+
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars -- for testing only */
+const enumeration = ["foo", "bar", "baz"] as const;
+
+export default defineComponent({
+    props: {
+        value: {
+            type: String as PropType<(typeof enumeration)[number]>,
+            required: true,
+        },
+    },
+});
+</script>
