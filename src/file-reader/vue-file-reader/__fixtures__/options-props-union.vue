130c22453b841adfd74e5d4353501eaf9849df96
David Sveningsson <ext@sidvind.com>
2025-12-19 17:10:25 +0100

    fix: fix union prop types not being displayed properly

---
+++ src/file-reader/vue-file-reader/__fixtures__/options-props-union.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { type PropType } from "vue";
+
+export default {
+    props: {
+        types: {
+            type: [String, Number] as PropType<string | number>,
+            required: true,
+        },
+        enumeration: {
+            type: String as PropType<"active" | "inactive" | "pending">,
+            required: true,
+        },
+        complex: {
+            type: Object as PropType<{ foo: "spam" } | { foo: "ham" }>,
+            required: true,
+        },
+    },
+};
+</script>
+
+<template>
+    <div></div>
+</template>
