fd270e439f811fd1147548fd12fe5f3f22d10559
David Sveningsson <ext@sidvind.com>
2026-03-07 15:37:55 +0100

    refactor(eslint): handle `unicorn/prefer-type-error`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -101,7 +101,6 @@ export default [
         name: "local/technical-debt",
         rules: {
             "sonarjs/slow-regex": "off",
-            "unicorn/prefer-type-error": "off",
             "unicorn/text-encoding-identifier-case": "off",
         },
     },
