bf0160fd8ff3028b26616382e6e847be30f8e8c6
Ferm Stefan (66132752) <stefan.ferm@forsakringskassan.se>
2026-04-24 11:46:56 +0200

    feat: new mobile layout (refs SFKUI-7490)
    fix: remove more item logic (refs SFKUI-7490)

--- src/runtime/header-layout-detector.ts
+++ src/runtime/header-layout-detector.ts
@@ -10,9 +10,10 @@
  *
  * @param value - The pixel string to be parsed (e.g., "16px" or "").
  * @param defaultValue - The fallback value to return if the input string is empty.
+ * @internal
  * @returns The parsed number representation of the pixel value, or the default value.
  */
-function parsePx(value: string, defaultValue: number): number {
+export function parsePx(value: string, defaultValue: number): number {
     const parsed = Number(value.replace("px", ""));
     if (Number.isNaN(parsed)) {
         return defaultValue;
@@ -34,20 +35,59 @@ function getContentWidth(header: HTMLDivElement): number {
     return width - (headerPaddingLeft + headerPaddingRight);
 }
 
+function getTopnavMaxWidht(right: HTMLElement): number {
+    // let width = 0;
+    const rightSlot = right.querySelector(".docs-page-header__right-slot");
+    const rightSlotWidth = rightSlot
+        ? rightSlot.getBoundingClientRect().width
+        : 0;
+    const rightWidth = right.getBoundingClientRect().width;
+    const rightStyle = getComputedStyle(right);
+    const rightGap = parsePx(rightStyle.gap, 0);
+
+    return rightWidth - rightSlotWidth - rightGap;
+}
+
+/**
+ * Calculates total item width by summing up pre-measured rects.
+ */
+function getMenuItemsWidth(items: HTMLElement[]): number {
+    return items.reduce(
+        (total, item) => total + item.getBoundingClientRect().width,
+        0,
+    );
+}
+
+function setMenuItemVisibility(items: HTMLElement[], visible: boolean): void {
+    const visibility = visible ? "visible" : "hidden";
+    for (const item of items) {
+        item.style.visibility = visibility;
+    }
+}
+
 /**
  * totalNeeded calculates the total width required to fit everything in the header on a single row.
  * headerWidth is the current total width of the header.
  */
-function checkLayout(): void {
+function checkLayout(nav: HTMLElement): void {
     /* eslint-disable @typescript-eslint/no-non-null-assertion -- let it crash at runtime if these doesn't actually exist */
     const header = document.querySelector<HTMLDivElement>(
         "header .docs-page-header",
     )!;
     const left = document.querySelector(".docs-page-header__left")!;
-    const right = document.querySelector(".docs-page-header__right")!;
+    const right = document.querySelector<HTMLElement>(
+        ".docs-page-header__right",
+    )!;
+    const mobileMeny = document.querySelector(".docs-mobile-nav")!;
+
+    const menuItems = Array.from(
+        nav.querySelectorAll<HTMLElement>(".docs-topnav__item"),
+    );
     /* eslint-enable @typescript-eslint/no-non-null-assertion */
 
     header.classList.remove("is-multi-row", "is-single-row");
+    mobileMeny.classList.remove("is-mobile");
+    nav.classList.remove("is-mobile");
 
     const leftWidth = left.getBoundingClientRect().width;
     const rightWidth = right.getBoundingClientRect().width;
@@ -64,6 +104,19 @@ function checkLayout(): void {
 
     header.classList.toggle("is-multi-row", !headerFit);
     header.classList.toggle("is-single-row", headerFit);
+
+    // In multi-row mode, check if the topnav menu fits; if not, toggle to the mobile layout.
+    if (!headerFit) {
+        const topnavMaxWidth = getTopnavMaxWidht(right);
+        const topnavWidth = getMenuItemsWidth(menuItems);
+
+        const topnavFit = topnavWidth < topnavMaxWidth;
+
+        mobileMeny.classList.toggle("is-mobile", !topnavFit);
+        nav.classList.toggle("is-mobile", !topnavFit);
+
+        setMenuItemVisibility(menuItems, topnavFit);
+    }
 }
 
 /**
@@ -72,14 +125,14 @@ function checkLayout(): void {
  * Attaches a mutation observer to the document.
  * Calls checkLayout to perform calculations for the header.
  */
-export function headerLayoutDetector(): void {
+export function headerLayoutDetector(nav: HTMLElement): void {
     const resizeObserver = new ResizeObserver(() => {
-        checkLayout();
+        checkLayout(nav);
     });
     document.documentElement.classList.remove("loading");
     resizeObserver.observe(document.body);
     window.addEventListener("docs:navigation", () => {
-        checkLayout();
+        checkLayout(nav);
     });
-    checkLayout();
+    checkLayout(nav);
 }
