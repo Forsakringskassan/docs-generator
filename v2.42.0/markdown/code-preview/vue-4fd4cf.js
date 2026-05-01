0536aec205fb5b40f899e911ed665b4c4f4f254e
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2026-05-01 00:30:22 +0000

    chore(release): [skip ci] deploy v2.42.0 documentation

--- v2.42.0/markdown/code-preview/vue-4fd4cf.js
+++ v2.42.0/markdown/code-preview/vue-4fd4cf.js
@@ -10,32 +10,25 @@ function setup(options) {
   app.mount(selector);
 }
 
-// virtual-entry:virtual:docs/markdown/files/MyFile.vue:MyFile-d3717c.js
-import { ref } from "vue";
+// virtual-entry:virtual:docs/markdown/code-preview/vue.md:vue-4fd4cf.js
 import { toDisplayString as _toDisplayString, openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
+var name = "World";
 var exampleComponent = {
-  __name: "MyFile",
+  __name: "vue",
   setup(__props, { expose: __expose }) {
     __expose();
-    const name = ref("world");
-    const __returned__ = { name, ref };
+    const __returned__ = { name };
     Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
     return __returned__;
   }
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return _openBlock(), _createElementBlock(
-    "div",
-    null,
-    "Hello " + _toDisplayString($setup.name) + "!",
-    1
-    /* TEXT */
-  );
+  return _openBlock(), _createElementBlock("p", null, "Hello " + _toDisplayString($setup.name) + "!");
 }
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-d3717c"
+  selector: "#example-4fd4cf"
 });
 export {
   render
