9b1566a3a3c4462f0614c767341e7cc663f3fa2c
David Sveningsson <ext@sidvind.com>
2026-06-29 22:43:12 +0200

    chore(deps): update @forsakringskassan/eslint-config monorepo

--- src/utils/get-fingerprint.ts
+++ src/utils/get-fingerprint.ts
@@ -1,6 +1,5 @@
 import crypto from "node:crypto";
 
 export function getFingerprint(source: string): string {
-    /* eslint-disable-next-line sonarjs/hashing -- technical debt, not used in a sensitive context but could still be replaced */
     return crypto.createHash("md5").update(source).digest("hex").slice(0, 6);
 }
