bfa2d0dd647f9f57c7fc35b2480ddfd06f3fef25
ext <ext@users.noreply.github.com>
2026-07-23 08:43:11 +0000

    Deploy preview for PR 456 🛫

---
+++ pr-preview/pr-456/docs-generator/markdown/api/vue-8168a4.js
@@ -0,0 +1,40 @@
+// docs/src/setup.ts
+import { createApp, h } from "vue";
+function setup(options) {
+  const { rootComponent, selector } = options;
+  const app = createApp({
+    render() {
+      return h(rootComponent);
+    }
+  });
+  app.mount(selector);
+}
+
+// virtual-entry:virtual:docs/markdown/api/vue.md:vue-8168a4.js
+import { toDisplayString as _toDisplayString, openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue";
+var exampleComponent = {};
+function render(_ctx, _cache) {
+  return _openBlock(), _createElementBlock(
+    "p",
+    null,
+    _toDisplayString(
+      /** something something... */
+      _ctx.$t("translation.key.bar", "bar default text", {
+        /** A thingamajig */
+        foo: 1,
+        /** A doodad */
+        bar: "baz"
+      })
+    ),
+    1
+    /* TEXT */
+  );
+}
+exampleComponent.render = render;
+setup({
+  rootComponent: exampleComponent,
+  selector: "#example-8168a4"
+});
+export {
+  render
+};
